7347f1e5eb52ec8d36ad2151961b4cf28e2dd536
romain344 <romainleoulzec@gmail.com>
2026-04-09 11:26:28 +0200

    Add canUnlockAll function to solve lockboxes problem

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -7,8 +7,8 @@ Module to check if all boxes can be opened
 def canUnlockAll(boxes):
     """Determines if all boxes can be opened"""
     n = len(boxes)
-    opened = set([0])  # boîtes ouvertes
-    keys = [0]  # clés à explorer
+    opened = set([0])
+    keys = [0]
 
     while keys:
         current = keys.pop()
