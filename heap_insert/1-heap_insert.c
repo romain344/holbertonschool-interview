fc4f3b5f8d2a0ffcaef841c67a443a8f0e7119b9
romain344 <romainleoulzec@gmail.com>
2026-04-24 18:03:50 +0200

    fix for betty validy

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -1,6 +1,40 @@
 #include "binary_trees.h"
 
 /**
+ * bfs_insert - Inserts new_node at next available position using BFS
+ * @root: Root of the heap
+ * @new_node: Node to insert
+ */
+static void bfs_insert(heap_t *root, heap_t *new_node)
+{
+	heap_t *queue[1024];
+	heap_t *parent;
+	int front, rear;
+
+	front = 0;
+	rear = 0;
+	queue[rear++] = root;
+	while (front < rear)
+	{
+		parent = queue[front++];
+		if (!parent->left)
+		{
+			parent->left = new_node;
+			new_node->parent = parent;
+			return;
+		}
+		queue[rear++] = parent->left;
+		if (!parent->right)
+		{
+			parent->right = new_node;
+			new_node->parent = parent;
+			return;
+		}
+		queue[rear++] = parent->right;
+	}
+}
+
+/**
  * heap_insert - Inserts a value in a Max Binary Heap
  * @root: Double pointer to the root node of the heap
  * @value: Value to insert
@@ -9,8 +43,8 @@
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *new_node, *parent, *queue[1024];
-	int front, rear, tmp;
+	heap_t *new_node;
+	int tmp;
 
 	if (!root)
 		return (NULL);
@@ -25,27 +59,7 @@ heap_t *heap_insert(heap_t **root, int value)
 		return (new_node);
 	}
 
-	front = 0;
-	rear = 0;
-	queue[rear++] = *root;
-	while (front < rear)
-	{
-		parent = queue[front++];
-		if (!parent->left)
-		{
-			parent->left = new_node;
-			new_node->parent = parent;
-			break;
-		}
-		queue[rear++] = parent->left;
-		if (!parent->right)
-		{
-			parent->right = new_node;
-			new_node->parent = parent;
-			break;
-		}
-		queue[rear++] = parent->right;
-	}
+	bfs_insert(*root, new_node);
 
 	while (new_node->parent && new_node->n > new_node->parent->n)
 	{
